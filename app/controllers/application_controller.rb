221b283f3b84215059cc062defc74e5cc9d80fe6
tunnelkowai <ueda1410013@yahoo.co.jp>
2022-07-21 01:07:35 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      p(p(username) == p(ENV["BASIC_AUTH_USER"])) && p(p(password) == p(ENV["BASIC_AUTH_PASSWORD"]))
+    end
+  end
+
 end
